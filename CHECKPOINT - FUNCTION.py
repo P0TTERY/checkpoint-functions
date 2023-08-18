ed16ada7da1c907d88ff18f1b4202b5c6ef3c35e
P0TTERY <142548048+P0TTERY@users.noreply.github.com>
2023-08-18 17:05:15 +0100

    Add files via upload

---
+++ CHECKPOINT - FUNCTION.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
+
+def main():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operator = input("Enter the operator (+, -, *, /): ")
+
+        if operator == "+":
+            result = add(num1, num2)
+        elif operator == "-":
+            result = subtract(num1, num2)
+        elif operator == "*":
+            result = multiply(num1, num2)
+        elif operator == "/":
+            result = divide(num1, num2)
+        else:
+            print("Invalid operator")
+            return
+
+        print("The result is:", result)
+    except ValueError as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
+
+
+
+
